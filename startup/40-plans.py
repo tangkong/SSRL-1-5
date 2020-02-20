57c824eff9264dc75d88b62b3accca07bb7726d6
tangkong <tangkongrobert@gmail.com>
2020-02-19 16:10:18 -0800

    Comments on plan usage

--- startup/40-plans.py
+++ startup/40-plans.py
@@ -1,2 +1,5 @@
 from ssrltools.plans import meshcirc, nscan
 
+
+# For scanning over all sample locations, try bp.list_scan or bp.list_grid_scan
+# eg: bp.list_scan([dets], HiTpStage.sample_loc_list(), md={'sample': 'sample name', ...})
