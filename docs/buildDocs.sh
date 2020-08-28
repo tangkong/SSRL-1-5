756e0f33d092c95ebb315757675930bfbd745a9e
tangkong <tangkongrobert@gmail.com>
2020-08-27 17:05:44 -0700

    update ipython version

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -18,7 +18,7 @@ set -x
 ###################
  
 apt-get update
-apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme ipython
+apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme ipython=7.15.0
  
 #####################
 # DECLARE VARIABLES #
