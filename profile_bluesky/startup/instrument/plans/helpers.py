b51a366168123e768ba576498ae8a71f952f053a
tangkong <tangkongrobert@gmail.com>
2020-09-23 13:56:19 -0700

    add show_image()

--- profile_bluesky/startup/instrument/plans/helpers.py
+++ profile_bluesky/startup/instrument/plans/helpers.py
@@ -2,7 +2,15 @@
 Helper plans, functions
 
 """
+from ..framework.initialize import db
 
 def show_table(ind=-1):
     return db[ind].table()
 
+def show_image(ind=-1, data_pt=1, img_key='marCCD_image'): 
+    # Try databroker v2 maybe, at some point
+    arr = db[ind].table(fill=True)[img_key][data_pt][0]
+    vmax = np.mean(arr)+3*np.std(arr)
+
+    plt.imshow(arr, vmax=vmax)
+    plt.colorbar()
