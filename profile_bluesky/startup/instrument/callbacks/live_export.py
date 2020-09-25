1ef87b22ebf28fcf01ca3630d0c7fe9d773159b2
tangkong <tangkongrobert@gmail.com>
2020-09-24 20:01:43 -0700

    live export selective tiff export

--- profile_bluesky/startup/instrument/callbacks/live_export.py
+++ profile_bluesky/startup/instrument/callbacks/live_export.py
@@ -17,22 +17,22 @@ from event_model import Filler
 from databroker.core import discover_handlers
 
 __all__ = ['csv_rr', 'tiff_rr', 'meta_rr']
-
-f = Filler(discover_handlers())
+hands = discover_handlers()
+hands.pop("XSP3")
 
 def csv_factory(name, start_doc):
     serializer = sc.Serializer( 
         '/home/b_spec/export/csv/', file_prefix='Scan{start[scan_id]}-' )
 
     def cb(name, doc):
+        f(name,doc)
         serializer(name, doc) 
 
     return [cb], []
 
 csv_rr = RunRouter([csv_factory])
 
-callback_db['csv_rr'] = RE.subscribe(csv_rr)
-
+#callback_db['csv_rr'] = RE.subscribe(csv_rr)
 
 class Selector(DocumentRouter):
     def __init__(self, exclude=None, **kwargs):
@@ -40,43 +40,58 @@ class Selector(DocumentRouter):
         super().__init__(**kwargs)
 
     def start(self, doc):
+        #print('---start')
         self.emit('start', doc)
 
     def descriptor(self, doc):
-        edited = copy.deepcopy(doc)
+        #print('---desc')
+        edited = copy.deepcopy(dict(doc))
         for key in self._exclude:
             edited['data_keys'].pop(key, None)
         self.emit('descriptor', edited)
 
     def event(self, doc):
-        edited = copy.deepcopy(doc)
-        f('event', edited)
+        #print('---event')
+        #print(doc)
+        edited = copy.deepcopy(dict(doc))
         for key in self._exclude:
             edited['data'].pop(key, None)
             edited['timestamps'].pop(key, None)
+            edited.get('filled', {}).pop(key, None)
+        #print('---event: emitting to serializer')
         self.emit('event', edited)
-    
+
+    def resource(self, doc):
+        self.emit('resource', doc)
+
+    def datum(self, doc):
+        self.emit('datum', doc)
+
     def stop(self, doc):
         self.emit('stop', doc)
 
-
 def tiff_factory(name, start_doc):
     serializer = st.Serializer( 
-        '/home/b_spec/export/tiff/', file_prefix='Scan{start[scan_id]}-' )
+        '/home/b_spec/export/tiff/', file_prefix='Scan{start[scan_id]}-')
+
+    f = Filler(hands)
+    #serializer('start', start_doc)
+    def cb(name, doc):
+        name_, doc_, = f(name, doc)
+        serializer(name_, doc_)
 
     selector = Selector(exclude=['xsp3_channel1', 'xsp3_channel2'], 
-                        emit=serializer)
-
+                        emit=cb)
+    
     selector._stashed_hard_ref_to_serializer = serializer
+    selector._stashed_hard_ref_to_filler = f 
+    selector._stashed_hard_ref_to_cb = cb
     return [selector], []
 
-
-    return [cb], []
-
-tiff_rr = RunRouter([tiff_factory], discover_handlers())
+assert 'AD_TIFF' in hands
+tiff_rr = RunRouter([tiff_factory])
 
 callback_db['tiff_rr'] = RE.subscribe(tiff_rr)
-
 
 def meta_factory(name, start_doc):
     serializer = sj.Serializer( 
