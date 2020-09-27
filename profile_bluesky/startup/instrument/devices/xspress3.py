55e93e1e67413cdba1ead35a67fdf49ca85c1ea4
tangkong <tangkongrobert@gmail.com>
2020-09-27 15:51:17 -0700

    fix ROI's

--- profile_bluesky/startup/instrument/devices/xspress3.py
+++ profile_bluesky/startup/instrument/devices/xspress3.py
@@ -141,7 +141,7 @@ xsp3.settings.configuration_attrs = ['acquire_period',
                         'trigger_signal']
 
 for n, d in xsp3.channels.items():
-    roi_names = ['roi{:02}'.format(j) for j in [1, ]]
+    roi_names = ['roi{:02}'.format(j) for j in [1, 2, 3]]
     d.rois.read_attrs = roi_names
     d.rois.configuration_attrs = roi_names
     for roi_n in roi_names:
